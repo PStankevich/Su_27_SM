5bde1607714b2af28736581ac222f798d2271972
sliver449 <sliver449@gmail.com>
2016-07-29 12:47:10 +0300

    Рализована индикация закрылков в зависимости от типа самолета

--- Arduino/Su_27_SM/Su_27_SM/Su_27_SM.ino
+++ Arduino/Su_27_SM/Su_27_SM/Su_27_SM.ino
@@ -16,7 +16,13 @@ boolean flag = true;
 int in;
 String Data;
 String DataNum;
-int gear = 5;
+int gearWarning = 11;
+int gear = 13;
+int airBreak = 12;
+int Noseflap = 2;
+int flaps = 10;
+int flapsLand = 9;
+int masterWarning = 3;
 int indexCount = 0;
 int charIndex = 0;
 int charCount = 0;
@@ -30,6 +36,7 @@ void setup() {
 	pinMode(12, OUTPUT);
 	pinMode(11, OUTPUT);
 	pinMode(10, OUTPUT);
+	pinMode(9, OUTPUT);
 	pinMode(2, OUTPUT);
 	pinMode(3, OUTPUT);
 	while (!Serial) {
@@ -73,40 +80,59 @@ void loop() {
 				indexCount = charIndex + 1;
 			}
 			if (data[0] > 0 && data[0] < 1.0) {
-				digitalWrite(11, HIGH);
+				digitalWrite(gearWarning, HIGH);
 			}
 			else {
-				digitalWrite(11, LOW);
+				digitalWrite(gearWarning, LOW);
 			} 
 			if (data[0] == 1) {
-				digitalWrite(13, HIGH);
+				digitalWrite(gear, HIGH);
 			}
 			else {
-				digitalWrite(13, LOW);
+				digitalWrite(gear, LOW);
 			}
-			if (data[1] == 1) {
-				digitalWrite(10, HIGH);
+			if (data[5] == 1) {
+				if (data[1] >= 0.24) {
+					digitalWrite(flaps, HIGH);
+				}
+				else {
+					digitalWrite(flaps, LOW);
+				}
+					if (data[1] >= 0.93) {
+						digitalWrite(flapsLand, HIGH);
+					}
+					else {
+						digitalWrite(flapsLand, LOW);
+					}
 			}
-			else {
-				digitalWrite(10, LOW);
+			else if (data[5] == 2) {
+				if (data[1] >= 1) {
+					digitalWrite(flaps, HIGH);
+				}
+				else {
+					digitalWrite(flaps, LOW);
+				}
 			}
+			
+
 			if (data[2] == 1) {
-				digitalWrite(12, HIGH);
+				digitalWrite(airBreak, HIGH);
 			}
 			else {
-				digitalWrite(12, LOW);
+				digitalWrite(airBreak, LOW);
 			}
-			if (data[3] >= 23) {
-				digitalWrite(2, HIGH);
-			}
-			else {
-				digitalWrite(2, LOW);
-			}
+				if (data[3] >= 23) {
+					digitalWrite(Noseflap, HIGH);
+				}
+				else {
+					digitalWrite(Noseflap, LOW);
+				}
+			
 			if (data[4] == 1) {
-				digitalWrite(3, HIGH);
+				digitalWrite(masterWarning, HIGH);
 			}
 			else {
-				digitalWrite(3, LOW);
+				digitalWrite(masterWarning, LOW);
 			}
 
 			Data = "";
